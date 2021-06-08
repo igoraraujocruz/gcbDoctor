3b8e10d23ae433b15fdd2e22ffc124611189b6ac
Igor Araujo Cruz <igoraraujocruzz@gmail.com>
2021-06-08 16:18:06 -0300

    chore: add dotenv

--- ormconfig.js
+++ ormconfig.js
@@ -1,14 +1,17 @@
+require('dotenv/config');
+
+const dir = process.env.NODE_ENV === 'dev' ? 'src' : 'dist';
+const file = process.env.NODE_ENV === 'dev' ? 'ts' : 'js';
+
 module.exports = {
-  type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: 'gcb',
-  database: 'gcbdoctor',
-  migrations: [
-    "./src/database/migrations/*.ts"
-  ],
-  cli: {
-    "migrationsDir": "./src/typeorm/migrations"
-  }
+    type: process.env.DB_TYPE,
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    migrations: [`./${dir}/shared/infra/typeorm/migrations/*.${file}`],
+    cli: {
+        "migrationsDir": `./${dir}/shared/infra/typeorm/migrations/`
+    }
 }
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,16 @@
 import './shared/infra/typeorm';
 import express from 'express';
 
+require('dotenv/config');
+
 const app = express();
 
-app.get('/', (request, response) => response.json({ message: 'Hello World' }));
+app.get('/', (request, response) =>
+  response.json({
+    message: process.env.APP_NAME,
+  }),
+);
 
-app.listen(3333, () => {
-  console.log('Server started on port 3333!');
+app.listen(process.env.APP_PORT, () => {
+  console.log(`Server started on port ${process.env.APP_PORT}`);
 });
