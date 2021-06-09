e2c0f641531c554299670dfba9314978d320656f
Igor Araujo Cruz <igoraraujocruzz@gmail.com>
2021-06-09 11:53:03 -0300

    feat: add celebrate library

--- src/modules/doctors/infra/http/routes/doctors.routes.ts
+++ src/modules/doctors/infra/http/routes/doctors.routes.ts
@@ -1,10 +1,40 @@
 import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
 import DoctorsController from '@modules/doctors/infra/http/controllers/DoctorsController';
 
 const doctorsRouter = Router();
 const doctorsController = new DoctorsController();
 
-doctorsRouter.post('/', doctorsController.create);
-doctorsRouter.put('/:id', doctorsController.update);
+doctorsRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      crm: Joi.string().required(),
+      landline: Joi.string().required(),
+      mobile_phone: Joi.string().required(),
+      zip_code: Joi.string().required(),
+      medical_specialty: Joi.string().required(),
+    },
+  }),
+  doctorsController.create,
+);
+doctorsRouter.put(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      crm: Joi.string().required(),
+      landline: Joi.string().required(),
+      mobile_phone: Joi.string().required(),
+      zip_code: Joi.string().required(),
+      medical_specialty: Joi.string().required(),
+    },
+  }),
+  doctorsController.update,
+);
 
 export default doctorsRouter;
