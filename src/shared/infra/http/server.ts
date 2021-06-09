bb99fb01114fa96725a44f991dcd39e322733d7d
Igor Araujo Cruz <igoraraujocruzz@gmail.com>
2021-06-09 12:30:06 -0300

    feat: add global exception handler

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,12 +1,42 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
 import '@shared/infra/typeorm';
 import '@shared/container';
 import routes from '@shared/infra/http/routes';
+import AppError from '@shared/errors/AppError';
+import { isCelebrateError } from 'celebrate';
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
+app.use((error: Error, _: Request, response: Response, __: NextFunction) => {
+  if (error instanceof AppError) {
+    const { statusCode } = error;
+
+    return response.status(statusCode).json({
+      status: 'error',
+      message: error.message,
+    });
+  }
+
+  if (isCelebrateError(error)) {
+    const values = error.details.values();
+    let { message } = values.next().value.details[0];
+    message = message.replace('"', '').replace('"', '');
+
+    return response.status(400).json({
+      status: 'error',
+      type: 'validation',
+      message,
+    });
+  }
+
+  return response.status(500).json({
+    status: 'error',
+    message: error.message,
+  });
+});
 
 app.listen(process.env.APP_PORT, () => {
   console.log(`Server started on port ${process.env.APP_PORT}`);
