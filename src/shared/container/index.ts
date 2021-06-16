469c26026822e1ae8481cc331b1166ac9cb588cb
Igor Araujo Cruz <igoraraujocruzz@gmail.com>
2021-06-16 02:46:53 -0300

    feat: add doctor routes

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,8 +1,15 @@
 import { container } from 'tsyringe';
 import IDoctorsRepository from '@modules/doctors/repositories/IDoctorsRepository';
 import DoctorsRepository from '@modules/doctors/infra/typeorm/repositories/DoctorsRepository';
+import IMedicalSpecialtiesRepository from '@modules/doctors/repositories/IMedicalSpecialtiesRepository';
+import MedicalSpecialtiesRepository from '@modules/doctors/infra/typeorm/repositories/MedicalSpecialtiesRepository';
 
 container.registerSingleton<IDoctorsRepository>(
   'DoctorsRepository',
   DoctorsRepository,
+);
+
+container.registerSingleton<IMedicalSpecialtiesRepository>(
+  'MedicalSpecialtiesRepository',
+  MedicalSpecialtiesRepository,
 );
